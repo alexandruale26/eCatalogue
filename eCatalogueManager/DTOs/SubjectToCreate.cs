ac34d5cf5d2d1080bcbe2fdc90c06f0104aa9c18
alexandruale26 <alexandru_ale26@yahoo.com>
2022-10-08 12:38:52 +0300

    error1

---
+++ eCatalogueManager/DTOs/SubjectToCreate.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECatalogueManager.DTOs
+{
+    public class SubjectToCreate
+    {
+        [Required(ErrorMessage = "Subject name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "TeacherId ID is required")]
+        [Range(1, 1000, ErrorMessage = "ID cannot be less than 1 or greater than 1000")]
+        public int TeacherId { get; set; }
+    }
+}
---
+++ eCatalogueManager/DTOs/SubjectToGet.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ECatalogueManager.DTOs
+{
+    public class SubjectToGet
+    {
+        public string Name { get; set; }
+        public int TeacherId { get; set; }
+    }
+}
